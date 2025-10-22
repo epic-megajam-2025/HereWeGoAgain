689249e264a44ef316c2ce1c27bb6bf302476f1f
reddification <reddification@gmail.com>
2025-10-22 23:35:04 +0400

    Add knockable physics object

---
+++ Source/HereWeGoAgain/Actors/KnockablePhysicsObject.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KnockablePhysicsObject.h"
+
+#include "Components/AudioComponent.h"
+#include "Components/BoxComponent.h"
+#include "Perception/AISense_Hearing.h"
+
+
+// Sets default values
+AKnockablePhysicsObject::AKnockablePhysicsObject()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh Component"));
+	SetRootComponent(StaticMeshComponent);
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AudioComponent->SetupAttachment(RootComponent);
+	NavAreaBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("NavAreaBoxComponent"));
+	NavAreaBoxComponent->SetupAttachment(RootComponent);
+
+	StaticMeshComponent->SetCanEverAffectNavigation(false);
+	StaticMeshComponent->SetCollisionProfileName("Ragdoll");
+	StaticMeshComponent->SetSimulatePhysics(true);
+	NavAreaBoxComponent->SetCanEverAffectNavigation(true);
+	NavAreaBoxComponent->bDynamicObstacle = true;
+	AudioComponent->SetAutoActivate(false);
+}
+
+void AKnockablePhysicsObject::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	StaticMeshComponent->OnComponentHit.AddDynamic(this, &AKnockablePhysicsObject::OnObjectHit);
+}
+
+bool AKnockablePhysicsObject::IsFallen(float Z) const
+{
+	FRotator MeshRotation = StaticMeshComponent->GetComponentRotation();
+	return StaticMeshComponent->GetComponentVelocity().SizeSquared() > FallSpeedThreshold * FallSpeedThreshold
+		|| LastHitZ - Z > FallZHitThreshold && (FMath::Abs(MeshRotation.Pitch) > FallAngleThreshold || FMath::Abs(MeshRotation.Roll) > FallAngleThreshold);	
+}
+
+void AKnockablePhysicsObject::OnObjectHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!IsFallen(Hit.Location.Z))
+		return;
+
+	if (OtherActor == nullptr || OtherActor->IsA<APawn>())
+		return;
+
+	LastHitZ = Hit.Location.Z;
+	AudioComponent->Play();
+	UAISense_Hearing::ReportNoiseEvent(this, Hit.Location, AINoiseLoudness, this, AINoiseSoundRange, ObjectHitAISoundTag.GetTagName());
+}
---
+++ Source/HereWeGoAgain/Actors/KnockablePhysicsObject.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/Actor.h"
+#include "KnockablePhysicsObject.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class HEREWEGOAGAIN_API AKnockablePhysicsObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AKnockablePhysicsObject();
+
+protected:
+	virtual void PostInitializeComponents() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAudioComponent* AudioComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UBoxComponent* NavAreaBoxComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float FallZHitThreshold = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float FallAngleThreshold = 60.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float FallSpeedThreshold = 400.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag ObjectHitAISoundTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float AINoiseSoundRange = 650.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float AINoiseLoudness = 1.f;
+	
+private:
+	bool IsFallen(float Z) const;
+
+	UFUNCTION()
+	void OnObjectHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	float LastHitZ = FLT_MAX;
+};
